64e87dee5528709a67ed30dae9869573cef0f0f6
Armadio <86427952+armadio2902@users.noreply.github.com>
2022-12-29 20:57:46 +0100

    second commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,31 @@
-particlesJS.load('homeSectionBack', './js/particles.json', function() {
-    console.log('callback - particles.js config loaded');
+let getStartedButton = document.getElementById("scrollToCreateButton")
+let createSection = document.getElementById("createSectionBack")
+let fromDateInput = document.getElementById("fromDateInput")
+
+const date = new Date();
+
+// Wait for page to load
+window.addEventListener('load', () => {
+    //Add particles to homepage
+    particlesJS.load('homeSectionBack', './js/particles.json', function () {
+        console.log('callback - particles.js config loaded');
+    });
+
+    //Scroll into view when "Get started" button is pressed
+    getStartedButton.addEventListener("click", function () {
+        createSection.scrollIntoView();
+    })
+
+    //Set default fromDate to today
+    setCurrentDay()
 });
+
+// Functions
+
+function setCurrentDay() {
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    fromDateInput.value = `${year}-${month}-${day}`
+}
