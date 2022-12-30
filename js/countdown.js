2e3fcca18faf4b94496cca80b01fb29f197b89e6
Armadio <86427952+armadio2902@users.noreply.github.com>
2022-12-30 01:16:08 +0100

    made responsive ui for smartphones

---
+++ js/countdown.js
@@ -0,0 +1,94 @@
+let cdName = document.getElementById("cdName")
+let cdAuthor = document.getElementById("cdAuthor")
+let cdMessage = document.getElementById("cdMessage")
+let counter = document.getElementById("counter")
+
+window.addEventListener('load', () => {
+    // Check if the url has a parameter
+    hasQuery()
+
+    function hasQuery() {
+        let url = window.location.href;
+        var arr = url.split('?');
+        if (arr.length > 1 && arr[1] !== '') {
+            console.log('params found: decoding string and showing countdown');
+            let json = decode(arr[1])
+            show(json)
+            //Calculate timer every second
+            setInterval(function () {
+                // Get current date
+                let date = new Date();
+
+                // Get current minutes and hours
+                let minutes = date.getMinutes();
+                let hours = date.getHours();
+                
+                // Get final date
+                let finalDate = json.t
+                let finalYear = parseInt(finalDate.slice(0,4));
+                let finalMonth = parseInt(finalDate.slice(5,7))
+                let finalDay = parseInt(finalDate.slice(8,10))
+                // Format the final date (it's needed for the calculations)
+                let finalDateFormatted = new Date(`${finalYear}/${finalMonth}/${finalDay}`)
+
+                //Calculate minutes, hours and days left
+                let minutesLeft = 60 - minutes
+                let hoursLeft
+                if (minutes != 0) {
+                    hoursLeft = 23 - hours
+                } else {
+                    hoursLeft = 24 - hours
+                }
+                let msLeft = Math.abs(finalDateFormatted - date);
+                let daysLeft = Math.ceil(msLeft / (1000 * 60 * 60 * 24)); 
+                // Update the countdown
+                updateCD(daysLeft, hoursLeft, minutesLeft)
+            }, 1000)
+        } else {
+            console.log('params not found: cannot show countdown')
+            counter.textContent = "Error"
+            cdMessage.textContent = "Something went wrong"
+            document.getElementById("byLabel").style.display = "none"
+        }
+    }
+
+    function decode(param) {
+        let decodedData = atob(param)
+        let jsonData = JSON.parse(decodedData)
+        return jsonData
+    }
+
+    function show(jsonData) {
+        if (jsonData.n == "" || jsonData.t == "") {
+            counter.textContent = "Error"
+            cdMessage.textContent = "Something went wrong"
+        }
+        cdName.textContent = jsonData.n
+        if (jsonData.a != "") {
+            cdAuthor.textContent = jsonData.a
+        } else {
+            document.getElementById("byLabel").style.display = "none"
+        }
+
+        if (jsonData.m != "") {
+            cdMessage.textContent = "left until " + jsonData.m
+        } else {
+            cdMessage.textContent = "left"
+        }
+        
+    }
+
+    function updateCD(d, h, m) {
+        if (d < 10) {
+            d = "0" + d
+        }
+        if (h < 10) {
+            h = "0" + h
+        }
+        if (m < 10) {
+            m = "0" + m
+        }
+        let countdown = `${d}:${h}:${m}`
+        counter.textContent = countdown
+    }
+})
--- js/index.js
+++ js/index.js
@@ -1,31 +1,95 @@
 let getStartedButton = document.getElementById("scrollToCreateButton")
 let createSection = document.getElementById("createSectionBack")
+let urlSelection = document.getElementById("urlSectionBack")
 let fromDateInput = document.getElementById("fromDateInput")
+let toDateInput = document.getElementById("toDateInput")
+let messageInput = document.getElementById("messageInput")
+let createCountdownButton = document.getElementById("createCountdownButton")
+let countdownNameInput = document.getElementById("nameInput")
+let countdownAuthorInput = document.getElementById("authorInput")
+let copyUrlButton = document.getElementById("copyUrlButton")
+let urlOutput = document.getElementById("urlOutput")
 
 const date = new Date();
 
 // Wait for page to load
 window.addEventListener('load', () => {
+
+    // Check if everything is completed
+    setInterval(function() {
+        if (countdownNameInput.value != "" && toDateInput.value != "") {
+            // Enable create button 
+            createCountdownButton.disabled = false
+        } else {
+            // Disable create button 
+            createCountdownButton.disabled = true
+        }
+    }, 50)
+
     //Add particles to homepage
     particlesJS.load('homeSectionBack', './js/particles.json', function () {
         console.log('callback - particles.js config loaded');
     });
+
+    //Set default fromDate to today
+    setCurrentDay()
 
     //Scroll into view when "Get started" button is pressed
     getStartedButton.addEventListener("click", function () {
         createSection.scrollIntoView();
     })
 
-    //Set default fromDate to today
-    setCurrentDay()
+    //Create countdown 
+    createCountdownButton.addEventListener("click", function () {
+        createCountdown()
+    })
+
+    // copy to clipboard 
+    copyUrlButton.addEventListener("click", function () {
+        // Copy the text inside the text field
+        navigator.clipboard.writeText(urlOutput.textContent);
+
+        copyUrlButton.innerText = "Copied!"
+        
+        setTimeout(function() {
+            copyUrlButton.innerText = "Copy to clipboard"
+        }, 1000)
+    })
+
+
+    /**
+     * FUNCTIONS
+     */
+    function setCurrentDay() {
+        let day = date.getDate();
+        let month = date.getMonth() + 1;
+        let year = date.getFullYear();
+
+        fromDateInput.textContent = `${day}/${month}/${year}`
+    }
+
+    function createCountdown() {
+        let name = countdownNameInput.value
+        let author = countdownAuthorInput.value
+        let toDate = toDateInput.value
+        let message = messageInput.value
+
+        // insert data in a json string
+        let data = `
+    {
+        "n" : "${name}",
+        "a" : "${author}",
+        "t" : "${toDate}",
+        "m" : "${message}"
+    }`
+
+        // Encode the data using base64 
+        let encodedData = btoa(data)
+        urlSelection.style.display = "block"
+        urlSelection.scrollIntoView()
+        let hostname = window.location.host
+        urlOutput.textContent = "http://" + hostname + "/countdown.html" + "?" + encodedData
+    }
+
 });
 
-// Functions
-
-function setCurrentDay() {
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    let year = date.getFullYear();
-
-    fromDateInput.value = `${year}-${month}-${day}`
-}
